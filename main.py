1eee5522790edc688d9c37bfd441f214cc05539b
outpost-caprice <itani@outpost-corp.com>
2023-11-07 18:11:18 +0900

    modified:   main.py

--- main.py
+++ main.py
@@ -6,13 +6,17 @@ from datetime import datetime, timezone, timedelta
 import gspread
 from backoff import on_exception, expo
 from requests.exceptions import RequestException
+import time
 
 # 環境変数からスプレッドシートIDとクレデンシャルを取得
 SPREADSHEET_ID = os.getenv('SPREADSHEET_ID')
-GOOGLE_CREDENTIALS_BASE64 = os.getenv('GOOGLE_APPLICATION_CREDENTIALS_BASE64')
+GOOGLE_CREDENTIALS_BASE64 = os.getenv('CREDENTIALS_BASE64')
 
 # エラーハンドリング用の最大リトライ回数
-MAX_RETRIES = 5
+MAX_RETRIES = 3
+
+# 初回実行時フラグ
+_is_first_run = True
 
 # エラー発生時のバックオフ指数的戦略と最大再試行回数
 @on_exception(expo, RequestException, max_tries=MAX_RETRIES)
@@ -34,46 +38,71 @@ sheet = gc.open_by_key(SPREADSHEET_ID).sheet1
 HN_API_BASE = 'https://hacker-news.firebaseio.com/v0'
 
 def get_last_checked_id():
-    value = sheet.acell('B1').value
+    global _is_first_run
+    cell = 'S1' if _is_first_run else 'B1'
+    value = sheet.acell(cell).value
     if not value:
-        raise ValueError("B1セルに最新の記事IDが存在しません。")
+        raise ValueError(f"{cell}セルに最新の記事IDが存在しません。")
+    _is_first_run = False
     return int(value)
 
 def update_news_on_sheet(last_checked_id):
     maxitem = fetch_hn_api('maxitem')
-    if maxitem <= last_checked_id:
-        print(f"No new items to check. {maxitem} <= {last_checked_id}")
-        return
+    
+    if maxitem > last_checked_id:
+        latest_news_ids = fetch_hn_api('newstories')[:50]
+        
+        for count, news_id in enumerate(latest_news_ids):
+            if news_id <= last_checked_id:
+                continue
 
-    # /v0/newstories エンドポイントから最新50件の記事IDを取得
-    latest_news_ids = fetch_hn_api('newstories')
+            try:
+                news_data = fetch_hn_api(f'item/{news_id}')
+                if news_data and not news_data.get('dead'):
+                    write_news_to_sheet(news_data)
+            except Exception as e:
+                print(f"Non-fatal exception caught: {e}")
+            
+            time.sleep(1) # news_idごとに1秒間隔
+            if count % 10 == 9:
+                time.sleep(5) # 10個ごとに追加で5秒間隔
 
-    for news_id in reversed(latest_news_ids):
-        if news_id > last_checked_id:
-            news_data = fetch_hn_api(f'item/{news_id}')
-            if news_data and not news_data.get('dead'):
-                datetime_jst = datetime.utcfromtimestamp(news_data['time']) + timedelta(hours=9)
-                row = [
-                    datetime_jst.strftime('%Y-%m-%d %H:%M:%S'),
-                    news_data.get('title'),
-                    news_data.get('url'),
-                    news_id
-                ]
-                write_to_sheet_with_retry(row)
-    sheet.update('B1', maxitem)
+        last_checked_id = maxitem
+
+    # Update the last checked ID
+    sheet.update('B1', last_checked_id) 
+
+def write_news_to_sheet(news_data):
+    datetime_jst = datetime.utcfromtimestamp(news_data['time']) + timedelta(hours=9)
+    row = [
+        datetime_jst.strftime('%Y-%m-%d %H:%M:%S'),
+        news_data.get('title'),
+        news_data.get('url'),
+        news_data.get('id')
+    ]
+    # Insert the news data row into the spreadsheet, waiting 1 second before each write operation
+    write_to_sheet_with_retry(row)
+    time.sleep(1)
 
 @on_exception(expo, (gspread.exceptions.APIError, gspread.exceptions.GSpreadException), max_tries=MAX_RETRIES)
 def write_to_sheet_with_retry(row):
-    index = 2  # 新しいエントリを挿入する行のインデックス
-    sheet.insert_row(row, index=index)
+    try:
+        index = 2  # 新しいエントリを挿入する行のインデックス
+        sheet.insert_row(row, index=index)
+    except gspread.exceptions.APIError as e:
+        print(f"APIError encountered: {e}")
+    except gspread.exceptions.GSpreadException as e:
+        print(f"GSpreadException encountered: {e}")
 
 def check_and_update_new_hn_content(event, context):
     try:
+        print("Starting update process...")
         last_checked_id = get_last_checked_id()
         update_news_on_sheet(last_checked_id)
+        print("Update process completed.")
     except Exception as e:
         print(f"An error occurred: {e}")
-        raise  # 最新記事ID取得失敗など、致命的な問題が発生した場合はスクリプトを停止
+        raise
 
 if __name__ == "__main__":
     check_and_update_new_hn_content(None, None)
